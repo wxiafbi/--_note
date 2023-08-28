545934892753edefaad0a937b5f58d840be4f478
fbiawxi <wxiafbi@outlook.com>
2023-08-29 00:39:23 +0800

    1

---
+++ test.py
@@ -0,0 +1,2 @@
+print(i for i in range(3))
+
---
+++ tieba_spider.py
@@ -0,0 +1,12 @@
+class spider:
+    def __init__(self):
+        pass
+    def get_url_list(self):
+        pass
+
+
+    def run(self):
+        pass
+        '''1.获取url'''
+        '''2.get请求内容'''
+        '''3.保存网页'''
